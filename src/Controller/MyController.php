83c5790546d4656f79e5ec51d79f6859f2bde470
Glinglin <le.neko@gmail.com>
2019-02-20 00:40:06 +0100

    initial commit

---
+++ src/Controller/MyController.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MyController
+{
+    /**
+     * @Route("/", name="home", methods={"GET"})
+     */
+    public function show(Request $request): JsonResponse
+    {
+        $data = $this->resolve($request);
+
+        return new JsonResponse($data);
+    }
+
+    public function resolve(Request $request): array
+    {
+        $normalized = $this->normalizeIntegers(
+            $request->query->all()
+        );
+
+        return (new OptionsResolver())
+            ->setDefault('start', 0)
+            ->setAllowedTypes('start', 'int')
+            ->resolve($normalized);
+    }
+
+    public function normalizeIntegers(array $parameters): array
+    {
+        $normalized = [];
+
+        foreach($parameters as $key => $value){
+            $normalized[$key] = is_numeric($value) ? (int) $value : $value;
+        }
+
+        return $normalized;
+    }
+}
