6d1abeb522bbc7700dfb7c767c0804ae7eee3446
Glinglin <le.neko@gmail.com>
2019-02-21 22:53:01 +0100

    fix

--- src/Controller/TestTwigController.php
+++ src/Controller/TestTwigController.php
@@ -9,7 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class OtherController extends AbstractController
+class TestTwigController extends AbstractController
 {
     /**
      * @Route("/hello/{name}")
--- src/Controller/TestYamlController.php
+++ src/Controller/TestYamlController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Yaml\Yaml;
 
-class TestController extends AbstractController
+class TestYamlController extends AbstractController
 {
 
     /**
