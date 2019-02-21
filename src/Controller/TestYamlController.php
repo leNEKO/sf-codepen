e5c235dd023821d966a35596fb73aa707f32afc9
Glinglin <le.neko@gmail.com>
2019-02-22 00:34:03 +0100

    webgrind

--- src/Controller/TestYamlController.php
+++ src/Controller/TestYamlController.php
@@ -16,9 +16,15 @@ class TestYamlController extends AbstractController
     public function index(string $file)
     {
 
+        $hello = [];
+
+        for ($i = 0; $i < 100; $i++){
+            $hello[] = $i;
+        }
+
         $path = sprintf('%s/%s', __DIR__, 'hello.yaml');
         $hello = Yaml::parse(file_get_contents($path));
-        var_dump($hello);
+        dump($hello);
 
         $title = $path;
         $body = __DIR__;
@@ -28,4 +34,9 @@ class TestYamlController extends AbstractController
             'body' => $body,
         ]);
     }
+}
+
+
+if(!debug_backtrace()){
+    new TestYamController();
 }
